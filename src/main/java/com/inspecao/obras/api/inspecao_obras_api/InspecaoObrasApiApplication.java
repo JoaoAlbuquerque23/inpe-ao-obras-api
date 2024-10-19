46c693318df71610e164e0af346311d44dfc9abf
joao victor <jvc.albuquerque16@gmail.com>
2024-10-19 10:00:00 -0300

    ciração do projeto spring

---
+++ src/main/java/com/inspecao/obras/api/inspecao_obras_api/InspecaoObrasApiApplication.java
@@ -0,0 +1,13 @@
+package com.inspecao.obras.api.inspecao_obras_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspecaoObrasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspecaoObrasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inspecao/obras/api/inspecao_obras_api/InspecaoObrasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspecao.obras.api.inspecao_obras_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspecaoObrasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
